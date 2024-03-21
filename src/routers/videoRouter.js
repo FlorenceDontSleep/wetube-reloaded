b2f261db1fee6f458c39b5180ef69e516d64037b
FlorenceDontSleep <florence6151@gmail.com>
2024-03-22 00:13:18 +0900

    Router 업데이트
    Router에서 주소를 받는 부분을 hexadecimal 형태로 받게 변경

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -12,9 +12,11 @@ const videoRouter = express.Router();
 
 
 // (\\d+) -> digit 값만 받아온다
-videoRouter.get("/:id(\\d+)", watch);
+// ([0-9a-f]{24}) -> 24자의 길이의 0~9 , a~f의 문자열만 받아온다
+// 24자리 hexadecimal의 정규식(regular expression)
+videoRouter.get("/:id([0-9a-f]{24})", watch);
 // GET request를 받으면 getEdit으로 보내고, POST request를 받으면 postEdit으로 보냄
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 
