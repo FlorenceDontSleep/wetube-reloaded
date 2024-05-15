dbbd59e8dc29d11beb09bf4e761f20c8fe93d447
HS Kan <florence6151@gmail.com>
2024-05-16 00:11:46 +0900

    express-session 추가(~7.9)
    express-session 추가

--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
-
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
-
-
 
 const app = express();
 const logger = morgan("dev");
@@ -20,6 +18,27 @@ app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 //express가 form의 value를 js object로 확인할수 있게함
 app.use(express.urlencoded( {extended: true} ));
+
+app.use(
+    session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+    })
+);
+
+app.use((req, res, next) => {
+    req.sessionStore.all((error, sessions) => {
+        console.log(sessions);
+        next();
+    });
+});
+
+app.get("/add-one", (req, res, next) => {
+    req.session.potato += 1;
+    return res.send(`${req.session.id}\n${req.session.potato}`);
+})
+
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
